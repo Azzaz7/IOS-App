433ad6479ea89cd64c3f1b30f781adeaaeba004d
azzaz <azzaz@azzazs-Mac.local>
2018-03-09 16:42:48 +0200

    Initial Commit

---
+++ AsRect/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  AsRect
+//
+//  Created by azzaz on 3/9/18.
+//  Copyright © 2018 azzaz. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ AsRect/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AsRect
+//
+//  Created by azzaz on 3/9/18.
+//  Copyright © 2018 azzaz. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface ViewController : NSViewController
+
+
+@end
+
